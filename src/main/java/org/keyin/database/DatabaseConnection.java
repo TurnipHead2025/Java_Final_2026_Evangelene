8eab3cfef963efcc395a2e610575fe43c6c2ee52
TurnipHead2025 <Sherievangelene@gmail.com>
2026-08-07 09:20:32 -0230

    PostGres tables: User, Memberships, Workout_Class, Gym_Merch. Updated DatabaseConnection.java

--- src/main/java/org/keyin/database/DatabaseConnection.java
+++ src/main/java/org/keyin/database/DatabaseConnection.java
@@ -5,11 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    // You only need to change the name of the database in the URL, unless PG runs on another port on your system. Default port is 5432 
-    private static final String URL = "jdbc:postgresql://localhost:5432/s3javafinal";
-//    By default the username is postgres and the password is what ever you set it to be. I usually keep mine simple
+    
+    private static final String URL = "jdbc:postgresql://localhost:5432/gymdb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "software2026";
 
     public static Connection getConnection() throws SQLException, SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
