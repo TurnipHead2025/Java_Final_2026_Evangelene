0fc3899e1428c396b0e1fb14271995477dc22572
TurnipHead2025 <Sherievangelene@gmail.com>
2026-08-07 13:38:38 -0230

    UserService

--- src/main/java/org/keyin/user/UserService.java
+++ src/main/java/org/keyin/user/UserService.java
@@ -1,4 +1,40 @@
 package org.keyin.user;
 
+import java.sql.SQLException;
+import org.mindrot.jbcrypt.BCrypt;
+import java.util.List;
+
 public class UserService {
+    private UserDao userDao;
+
+    //constructor
+    public UserService() {
+    this.userDao = new UserDao();
+    }
+
+        //// Takes a User obj, hashes the password, sets the hashed password on the obj, then calls registerUser method from UserDao which inserts items into the DB
+    public void registerUser(User user) throws SQLException{
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        user.setPassword(hashedPassword);
+        userDao.registerUser(user);
+    }
+
+    // finds user by username from DB, then uses BCrypt to compare the entered password with the hashed one stored in DB. Returns the User obj if login successful, null if not
+    public User loginForUser(String username, String password) throws SQLException{
+        User user = userDao.getUserByUsername(username);
+        if (user != null && BCrypt.checkpw(password, user.getPassword())){
+            return user;
+        }
+        return null;
+    }
+
+    //Returns a list of all users- calls the UserDao getAllUsers method (Admin only)
+    public List<user> getAllUsers() throws SQLException{
+        return userDao.getAllUsers();
+    }
+
+    //Deletes a user by ID- calls UserDao deleteUser method (Admin Only)
+    public void deleteUser(int id) throws SQLException{
+        userDao.deleteUser(id);
+    }
 }
