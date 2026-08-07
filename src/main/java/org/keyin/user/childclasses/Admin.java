26e530e32fa3295606ac5cb9ec868a04fd933b06
TurnipHead2025 <Sherievangelene@gmail.com>
2026-08-07 08:47:31 -0230

    Admin, Trainer, and Member Classes

--- src/main/java/org/keyin/user/childclasses/Admin.java
+++ src/main/java/org/keyin/user/childclasses/Admin.java
@@ -3,4 +3,9 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Admin extends User {
+
+    //constuctor
+    public Admin(String userName, String password, String email, String address, String phoneNumber, String role){
+        super(userName, password, email, address, phoneNumber, "Admin");
+    }
 }
--- src/main/java/org/keyin/user/childclasses/Member.java
+++ src/main/java/org/keyin/user/childclasses/Member.java
@@ -3,4 +3,10 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Member extends User {
+
+    //constructor
+    //constuctor
+    public Member(String userName, String password, String email, String address, String phoneNumber, String role){
+        super(userName, password, email, address, phoneNumber, "Member");
+    }
 }
