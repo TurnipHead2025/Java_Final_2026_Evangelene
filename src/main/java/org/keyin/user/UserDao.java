178fe836104b13fee4a5586f96650d65016b7f3b
TurnipHead2025 <Sherievangelene@gmail.com>
2026-08-07 12:14:05 -0230

    User DAO

--- src/main/java/org/keyin/user/UserDao.java
+++ src/main/java/org/keyin/user/UserDao.java
@@ -1,30 +1,82 @@
 package org.keyin.user;
 
-import org.keyin.database.DatabaseConnection;
-
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.keyin.database.DatabaseConnection;
 
 public class UserDao {
 
+    //Finds a user in the DB by thier username. Creates and returns a new user obj if found, null if not found
     public User getUserByUsername(String username) throws SQLException {
-        String sql = "SELECT * FROM users WHERE user_name = ?";
-        DriverManager DatabaseConnector;
+        String sql = "SELECT * FROM users WHERE username = ?";
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, username);
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
-                    return new User(
-                            rs.getInt("user_id"),
-                            rs.getString("user_name"),
-                            rs.getString("user_password"),
-                            rs.getString("user_role")
-
+                    User user =  new User(
+                            rs.getString("username"),
+                            rs.getString("password"),
+                            rs.getString("email"),
+                            rs.getString("address"),
+                            rs.getString("phone"),
+                            rs.getString("role")
                     );
+                    user.setId(rs.getInt("id"));
+                    return user;
                 }
             }
         }
 
         return null;
+    }  
+    
+    // Inserts a user into the DB. The password should already be hashed before calling this method. 
+    public void registerUser(User user) throws SQLException{
+        String sql = "INSERT INTO users (username, password, email, address, phone, role) VALUES (?, ?, ?, ?, ?, ?)";
+        try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1, user.getUserName());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getEmail());  
+            pstmt.setString(4, user.getAddress());
+            pstmt.setString(5, user.getPhoneNumber());  
+            pstmt.setString(6, user.getRole());
+            pstmt.executeUpdate();                
+            }
+    }
+
+    //Creates and returns a list of all users in the Db (Admin only)
+    public List<User> getAllUsers() throws SQLException{
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery()){
+                while (rs.next()){
+                    User user = new User(
+                           rs.getString("username"),
+                            rs.getString("password"),
+                            rs.getString("email"),
+                            rs.getString("address"),
+                            rs.getString("phone"),
+                            rs.getString("role")
+                    );
+                    user.setId(rs.getInt("id"));
+                    users.add(user);
+                }
+            }
+        return users;    
+    }
+
+    //Deletes a user from the DB by their ID (Admin only)
+    public void deleteUser(int id) throws SQLException{
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();        
+            }
     }
 }
