02f6315ea39296cae5464352ec3314edac576b7a
TurnipHead2025 <Sherievangelene@gmail.com>
2026-08-09 08:08:51 -0230

    WorkoutClassService

--- src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
+++ src/main/java/org/keyin/workoutclasses/WorkoutClassService.java
@@ -1,9 +1,35 @@
 package org.keyin.workoutclasses;
 
 import java.sql.SQLException;
+import java.util.List;
 
-public class WorkoutClassService {
-    // Inject in the DAO to be able to use it in the service
+public class WorkoutClassService {    
     private WorkoutClassDAO workoutDAO;
 
+
+    //constructor
+    public WorkoutClassService(){
+        this.workoutDAO = new WorkoutClassDAO();
+    }
+
+    //methods
+
+    //Create class (Admin and Trainer)
+    public void createClass(WorkoutClass workoutClass) throws SQLException{
+        workoutDAO.createClass(workoutClass);
+    }
+
+    //Get all classes (Member only)
+    public List<WorkoutClass> getAllClasses() throws SQLException{
+        return workoutDAO.getAllClasses();
+    }
+
+    //Get classes by trainer id (Trainer Only)
+    public List<WorkoutClass> getClassesByTrainerId(int trainerId) throws SQLException {
+    return workoutDAO.getClassesByTrainerId(trainerId);
+}
+    //Delete Class by id (Admin and Trainer)
+    public int deleteClass(int id) throws SQLException{
+        return workoutDAO.deleteClasses(id);
+    }
 }
