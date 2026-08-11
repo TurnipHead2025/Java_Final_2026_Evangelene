7b6fa78c2b73d0ad3bb1f950c5fbbc17f3b81a64
TurnipHead2025 <Sherievangelene@gmail.com>
2026-08-11 09:49:11 -0230

    GymProductService

--- src/main/java/org/keyin/gymproduct/GymProductService.java
+++ src/main/java/org/keyin/gymproduct/GymProductService.java
@@ -1,8 +1,19 @@
 package org.keyin.gymproduct;
+import java.sql.SQLException;
+import java.util.List;
+
 
 public class GymProductService {
-    // Injecting the DAO for GymProduct gives us access to the database operations
-    // related to GymProduct entities.
     GymProductDAO gymProductDAO = new GymProductDAO();
+
+    //Add New Product (Admin Only)
+    public void addNewProduct(GymProduct gymProduct) throws SQLException{
+        gymProductDAO.addNewProduct(gymProduct);
+    }
+
+    //View Products for purchase (Trainer and Member). The valuation calculation is in the menu (Admin Only)
+    public List<GymProduct> getAllProducts() throws SQLException{
+        return gymProductDAO.getAllProducts();
+    }
 
 }
